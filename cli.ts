c324f6d8da4676a73416344e47283a695d67e765
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-11-29 20:44:59 +0200

    Improving the CLI integration

--- cli.ts
+++ cli.ts
@@ -1,77 +1,102 @@
 import * as child_process  from 'child_process';
+import * as chalk from 'chalk'
 
-export async function isAuthenticated(): Promise<boolean> {
-    try {
-        await execute('npm whoami')
-        return true
-        //return !stdout.includes('npm ERR! 401 Unauthorized - GET https://registry.npmjs.org/-/whoami')
-    } catch(e) {
-        //return !e.includes('npm ERR! 401 Unauthorized - GET https://registry.npmjs.org/-/whoami')
-        return false;
+export class PackageCli {
+    constructor(public name: string) {}
+
+    /**
+     * Retrieving if the NPM user is currently authenticated
+     */
+    async isAuthenticated(): Promise<boolean> {
+        try {
+            await this.execute('npm whoami')
+            return true
+        } catch(e) {
+            return false;
+        }
     }
-}
-
-/**
- * 
- * @param packageName 
- * @param cliArguments 
- */
-async function getCurrentVersion(packageName: string, cliArguments: string) {
-    const stdout = (await execute(`npm info ${packageName} version ${cliArguments}`)).stdout.replace('\n', '');
-    const split = stdout.split('.');
-	return {
-		major: parseInt(split[0]),
-		minor: parseInt(split[split.length-2]),
-		patch: parseInt(split[split.length-1])
-	}
-}
-
-/**
- * Retrieving the version of the current package
- * @param packageName The name of the package
- * @param cliArguments The additional CLI arguments
- */
-async function getUpgradeVersion(packageName: string, cliArguments: string): Promise<string> {
-    if(await doesPackageExist(packageName, cliArguments)) {
-        //const stdout = (await execute(`npm info ${packageName} version ${cliArguments}`)).stdout.replace('\n', '');
-        //const split = stdout.split('.');
-	    const version = await getCurrentVersion(packageName, cliArguments);
-	    if(version.patch < 9) version.patch++;
-	    else if(version.patch === 9 && version.minor < 9) {version.patch = 0; version.minor++}
-	    else if(version.patch === 9 && version.minor === 9 ) {version.patch = 0; version.minor = 0; version.major++;}
-        return `${version.major}.${version.minor}.${version.patch}`
+    /**
+     * Retrieving the current version of the package
+     * @param this.name The name of the package
+     * @param cliArguments The additional cli arguments
+     */
+    async getCurrentVersion(cliArguments: string) {
+        const stdout = (await this.execute(`npm info ${this.name} version ${cliArguments}`)).stdout.replace('\n', '');
+        const split = stdout.split('.');
+    	return {
+    		major: parseInt(split[0]),
+    		minor: parseInt(split[split.length-2]),
+    		patch: parseInt(split[split.length-1])
+    	}
     }
-    return '0.0.1';
-}
 
-/**
- * Checking if the pacakge exists in the relevant NPM registry
- * @param packageName The name of the package
- * @param cliArguments The additional CLI arguments
- */
-async function doesPackageExist(packageName: string, cliArguments: string): Promise<boolean> {
-    const arrayArguments = cliArguments.split(' ')
-    const isScopedRegistry = arrayArguments.findIndex((item: string) => item.includes('--registry') && !item.includes('registry.npmjs.org')) !== -1;
-    const isScope = arrayArguments.findIndex((item: string) => item.includes('--scope')) !== -1;
-    if(!isScopedRegistry && !isScope) {
-        const response = await execute(`npm search ${packageName} ${cliArguments}`);
-        return response.stdout.indexOf(`No matches found for "${packageName}"\n`) === -1;
+    /**
+     * Retrieving the version of the current package
+     * @param this.name The name of the package
+     * @param cliArguments The additional CLI arguments
+     */
+    async getUpgradeVersion(cliArguments: string): Promise<string> {
+        if(await this.doesPackageExist(cliArguments)) {
+    	    const version = await this.getCurrentVersion(cliArguments);
+    	    if(version.patch < 9) version.patch++;
+    	    else if(version.patch === 9 && version.minor < 9) {version.patch = 0; version.minor++}
+    	    else if(version.patch === 9 && version.minor === 9 ) {version.patch = 0; version.minor = 0; version.major++;}
+            return `${version.major}.${version.minor}.${version.patch}`
+        }
+        return '0.0.1';
     }
-    else {
-        console.log('Because of known NPM issues, we do not search for the package existence before deployment of Scoped packages.')
-        return true;
-    }
-}
 
-/**
- * Executes a shell command
- * @param command The command
- */
-function execute(command: string): Promise<{ stdout: string, stderr: string }> {
-    return new Promise((done, failed) => {
-        child_process.exec(command, (error, stdout, stderr) => {
-          if (error !== null) failed(error)
-          done({ stdout, stderr })
+    /**
+     * Checking if the pacakge exists in the relevant NPM registry
+     * @param this.name The name of the package
+     * @param cliArguments The additional CLI arguments
+     */
+    async doesPackageExist(cliArguments: string): Promise<boolean> {
+        const arrayArguments = cliArguments.split(' ')
+        const isScopedRegistry = arrayArguments.findIndex((item: string) => item.includes('--registry') && !item.includes('registry.npmjs.org')) !== -1;
+        const isScope = arrayArguments.findIndex((item: string) => item.includes('--scope')) !== -1;
+        if(!isScopedRegistry && !isScope) {
+            const response = await this.execute(`npm search ${this.name} ${cliArguments}`);
+            return response.stdout.indexOf(`No matches found for "${this.name}"\n`) === -1;
+        }
+        else {
+            console.log('Because of known NPM issues, we do not search for the package existence before deployment of Scoped packages.')
+            return true;
+        }
+    }
+
+    /**
+     * Upgrading package to next version
+     * @param cliArguments The additional CLI arguments
+     */
+    async upgradePackage(cliArguments: string): Promise<void> {
+        const version = this.getCurrentVersion(cliArguments);
+        const updateVersion = this.getUpgradeVersion(cliArguments);
+        console.log(`Upgrading ${this.name}@${version} to version ${this.name}@${version}`)
+        await this.execute(`npm version ${updateVersion} --allow-same-version ${cliArguments}`);
+        console.log(await this.execute(`npm publish ${cliArguments}`));
+    }
+
+    /**
+     * Executes a shell command
+     * @param command The command
+     */
+    async execute(command: string): Promise<{ stdout: string, stderr: string }> {
+        return new Promise((done, failed) => {
+            child_process.exec(command, (error, stdout, stderr) => {
+              if (error !== null) failed(error)
+              done({ stdout, stderr })
+            })
         })
-    })
+    }
+}
+
+/**
+ * Printing the help message
+ */
+export async function printHelp() {
+    console.log(chalk.magenta('In order to deploy a version, run the following command:'))
+    console.log(chalk.white('npm-deploy <packageName> <optional additional cli args>'))
+    console.log(chalk.white('for help, run npm-deploy --help'))
+    console.log(chalk.blueBright('If you liked our repo, please star it here https://github.com/danitseitlin/npm-package-deployer'))
 }
--- index.ts
+++ index.ts
@@ -1,78 +1,24 @@
 #!/usr/bin/env node
-import * as child_process  from 'child_process';
-import * as chalk from 'chalk'
-import { isAuthenticated } from './cli';
+import { PackageCli, printHelp } from './cli';
 
 (async () => {
     try {
         const cliArguments = process.argv.slice(3).join(' ');
-        const isNPMDeploy = (process.argv[1].includes('npm-deploy') || process.argv[1].includes('\\npm-package-deployer\\lib\\index.js'));
+        const isTriggered = (process.argv[1].includes('npm-deploy') || process.argv[1].includes('\\npm-package-deployer\\lib\\index.js'));
         //Verifying the deploy CLI command was executed
-        if(process.argv[0].includes('node') && isNPMDeploy === true && process.argv.length >= 3) {
+        if(process.argv[0].includes('node') && isTriggered && process.argv.length >= 3) {
             const packageName = process.argv[2];
-            if(!await isAuthenticated())
-                throw 'Auth is required!'
-            const version = await getVersion(packageName, cliArguments)
-            console.log(`Upgrading ${packageName} to version ${version}`)
-            await execute(`npm version ${version} --allow-same-version ${cliArguments}`);
-            console.log(await execute(`npm publish ${cliArguments}`));
+            if(packageName === '--help')
+                printHelp();
+            else {
+                const cli = new PackageCli(packageName)
+                if(!await cli.isAuthenticated())
+                    throw Error('Auth is required!')
+                await cli.upgradePackage(cliArguments);
+            }
         }
-        else console.log(chalk.yellow('Example: npm-deploy <package name>'))
+        else printHelp()
     } catch (e) {
         console.log(e)
     }
 })();
-
-/**
- * Retrieving the version of the current package
- * @param packageName The name of the package
- * @param cliArguments The additional CLI arguments
- */
-async function getVersion(packageName: string, cliArguments: string): Promise<string> {
-    if(await doesPackageExist(packageName, cliArguments)) {
-        const stdout = (await execute(`npm info ${packageName} version ${cliArguments}`)).stdout.replace('\n', '');
-        const split = stdout.split('.');
-	    const version = {
-	    	major: parseInt(split[0]),
-	    	minor: parseInt(split[split.length-2]),
-	    	patch: parseInt(split[split.length-1])
-	    }
-	    if(version.patch < 9) version.patch++;
-	    else if(version.patch === 9 && version.minor < 9) {version.patch = 0; version.minor++}
-	    else if(version.patch === 9 && version.minor === 9 ) {version.patch = 0; version.minor = 0; version.major++;}
-        return `${version.major}.${version.minor}.${version.patch}`
-    }
-    return '0.0.1';
-}
-
-/**
- * Checking if the pacakge exists in the relevant NPM registry
- * @param packageName The name of the package
- * @param cliArguments The additional CLI arguments
- */
-async function doesPackageExist(packageName: string, cliArguments: string): Promise<boolean> {
-    const arrayArguments = cliArguments.split(' ')
-    const isScopedRegistry = arrayArguments.findIndex((item: string) => item.includes('--registry') && !item.includes('registry.npmjs.org')) !== -1;
-    const isScope = arrayArguments.findIndex((item: string) => item.includes('--scope')) !== -1;
-    if(!isScopedRegistry && !isScope) {
-        const response = await execute(`npm search ${packageName} ${cliArguments}`);
-        return response.stdout.indexOf(`No matches found for "${packageName}"\n`) === -1;
-    }
-    else {
-        console.log('Because of known NPM issues, we do not search for the package existence before deployment of Scoped packages.')
-        return true;
-    }
-}
-
-/**
- * Executes a shell command
- * @param command The command
- */
-function execute(command: string): Promise<{ stdout: string, stderr: string }> {
-    return new Promise((done, failed) => {
-        child_process.exec(command, (error, stdout, stderr) => {
-          if (error !== null) failed(error)
-          done({ stdout, stderr })
-        })
-    })
-}
