7e70c1ccdae1f5dbafc186827e3e4795ab1b0788
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-03-11 22:44:55 +0200

    >

--- action.index.js
+++ action.index.js
@@ -6,6 +6,8 @@ const github_access_token = core.getInput('github_access_token');
 const npm_access_token = core.getInput('npm_access_token');
 const pkg_name = core.getInput('pkg_name');
 const pkg_registry = core.getInput('pkg_registry');
+const dry_run = core.getInput('dry_run')
+const pretty_print = core.getInput('pretty_print')
 
 function configureNPM(token, registry) {
     //Creating the .npmrc file
@@ -49,7 +51,7 @@ function configureGitHub(pkgName) {
  * Retrieving the version of the current package
  * @param cliArguments The additional CLI arguments
  */
- async function getUpgradeVersion(pkgName, registry) {
+async function getUpgradeVersion(pkgName, registry) {
     if(await doesPackageExist(pkgName, registry)) {
 	    const version = await getCurrentVersion(pkgName);
 	    if(version.patch < 9) version.patch++;
@@ -60,11 +62,22 @@ function configureGitHub(pkgName) {
     return '0.0.1';
 }
 
+/***
+ * 
+ */
+async function getCliArguments() {
+    let args = '';
+    if(pkg_registry)
+        args+= ` --registry=${pkg_registry}`;
+    if(dry_run === 'true' || dry_run === true)
+        args+= ` --dry-run`;
+}
+
 /**
  * Checking if the pacakge exists in the relevant NPM registry
  * @param cliArguments The additional CLI arguments
  */
- async function doesPackageExist(pkgName, cliArguments) {
+async function doesPackageExist(pkgName, cliArguments) {
     const arrayArguments = cliArguments.split(' ')
     const isScopedRegistry = arrayArguments.findIndex((item) => item.includes('--registry') && !item.includes('registry.npmjs.org')) !== -1;
     const isScope = arrayArguments.findIndex((item) => item.includes('--scope')) !== -1;
@@ -78,14 +91,58 @@ function configureGitHub(pkgName) {
     }
 }
 
+/**
+ * Parsing the publish output to a more pretified version
+ * @param output The publish output
+ */
+function parseDeployment(output) {
+    const split = output.stderr.split('\n');
+    const name = split.find(item => item.includes('name'))
+    const version = split.find(item => item.includes('version'))
+    const size = split.find(item => item.includes('package size'))
+    const unpackedSize = split.find(item => item.includes('unpacked size'))
+    const shasum = split.find(item => item.includes('shasum'))
+    const integrity = split.find(item => item.includes('integrity'))
+    const totalFiles = split.find(item => item.includes('total files'))
+    const files = []
+    const filesStartIndex = split.findIndex(item => item.includes('Tarball Contents'))
+    const filesEndIndex = split.findIndex(item => item.includes('Tarball Details'))
+    //Parsing only the files
+    for(let i = filesStartIndex+1; i < filesEndIndex; i++) {
+        files.push(split[i].split('B ')[1].replace(/ /g, '').replace('\n', ''));
+    }
+    //Building and returning the rest of the JS object
+    return {
+        files: files,
+        name: (name !== undefined) ? name.replace(/  /g, '').split(':')[1]: null,
+        version: (version !== undefined) ? version.replace(/  /g, '').split(': ')[1].replace(/ /g, ''): null,
+        size: (size !== undefined) ? size.replace(/  /g, '').split(':')[1]: null,
+        unpackedSize: (unpackedSize !== undefined) ? unpackedSize.replace(/  /g, '').split(': ')[1].replace(/ /g, ''): null,
+        shasum: (shasum !== undefined) ? shasum.replace(/  /g, '').split(':')[1]: null,
+        integrity: (integrity !== undefined) ? integrity.replace(/  /g, '').split(': ')[1]: null,
+        totalFiles: (totalFiles !== undefined) ? parseInt(totalFiles.replace(/  /g, '').split(': ')[1]): null,
+    }
+}
+
 (async () => {
     configureNPM(npm_access_token, pkg_registry);
     configureGitHub(pkg_name)
     const version = await getCurrentVersion(pkg_name)
     const updateVersion = await getUpgradeVersion(pkg_name, pkg_registry);
-    const cliArguments = `--registry=${pkg_registry}`
+    const cliArguments = getCliArguments();
     console.log(`Upgrading ${pkg_name}@${version.major}.${version.minor}.${version.patch} to version ${pkg_name}@${updateVersion}`)
     await execute(`npm version ${updateVersion} --allow-same-version ${cliArguments}`);
     const publish = await execute(`npm publish ${cliArguments} --dry-run`);
-    console.log(publish)
+    console.log('==== Publish Output ====')
+    if(pretty_print === 'true' || pretty_print === true) {
+        const prettyPublish = parseDeployment(publish);
+        const { files, ...rest } = prettyPublish
+        console.log(`files: ${files.toString().replace(/,/g, ', ')}`)
+        for(const item in rest) {
+            console.log(`${item}: ${rest[item].toString()}`)
+        }
+        console.log('========================')
+    }
+    else
+        console.log(publish)
 })();
