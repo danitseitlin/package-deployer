09f00a65d19bda55e9627f7834d68bf18613840d
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-03-14 20:15:51 +0200

    >

--- action.index.js
+++ action.index.js
@@ -35,7 +35,7 @@ async function configureGitHub(pkgName) {
  * @param {*} preRelease If the release is a pre-release
  */
 async function releaseGitHubVersion(version, branch, draft, preRelease) {
-    const repoURL = `https://github.com/${context.repo.owner}/${context.repo.repo}`;
+    const repoURL = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}`;
     const tagName = `v${version}`;
     const body = `Release of v${version}`;
     await execute(`curl --data '{"tag_name": "${tagName}","target_commitish": "${branch}","name": "${tagName}","body": "${body}","draft": ${draft},"prerelease": ${preRelease}' https://api.github.com/repos/${repoURL}/releases?access_token=${githubAccessToken}`)
