88be5af8308c10015aa81e836fa2cb875d78b06d
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-05-29 10:23:54 +0300

    Updating GitHub Action

--- dist/index.js
+++ dist/index.js
@@ -603,7 +603,10 @@ async function configureGitHub(pkgName) {
 async function releaseGitHubVersion(version, branch, draft, preRelease) {
     const tagName = `v${version}`;
     const body = `Release of v${version}`;
-    await execute(`curl -H 'Authorization: token ${githubAccessToken}' --data '{"tag_name": "${tagName}","target_commitish": "${branch}","name": "${tagName}","body": "${body}","draft": ${draft},"prerelease": ${preRelease}' https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/releases`)
+    if(debug)
+        console.log(`Releasing GitHub version ${tagName}`)
+    if(!dryRun)
+        await execute(`curl -H 'Authorization: token ${githubAccessToken}' --data '{"tag_name": "${tagName}","target_commitish": "${branch}","name": "${tagName}","body": "${body}","draft": ${draft},"prerelease": ${preRelease}' https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/releases`)
 }
 
 /**
