0fbe324bfd123a98a6cdbeb70821f517fc0572d0
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-05-30 00:52:21 +0300

    Updating GitHub Action

--- npm.index.js
+++ npm.index.js
@@ -22,7 +22,7 @@ const deployment = require('./src/deployment')
 (async () => {
     try {
         const cliArguments = process.argv.slice(3)//.join(' ');
-        const isTriggered = (process.argv[1].includes('deploy-pkg') || process.argv[1].includes('\\npm-package-deployer\\lib\\index.js'));
+        const isTriggered = (process.argv[1].includes('deploy-pkg') || process.argv[1].includes('\\npm-package-deployer\\lib\\npm.index.js'));
         //Verifying the deploy CLI command was executed
         if(process.argv[0].includes('node') && isTriggered && process.argv.length >= 3) {
             const packageName = process.argv[2];
