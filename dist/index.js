fe3f0cdfee7cca1cb1b1803e3c8754867f9d3e8f
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-05-31 00:38:48 +0300

    Updating GitHub Action

--- dist/index.js
+++ dist/index.js
@@ -6405,7 +6405,7 @@ async function releaseGitHubVersion(data) {
     if(!data.dryRun) {
         const res = await utils.execute(`curl -H 'Authorization: token ${data.token}' --data '{"tag_name": "${tagName}","target_commitish": "${data.branch}","name": "${tagName}","body": "${body}","draft": ${data.draft},"prerelease": ${data.preRelease}' https://api.github.com/repos/${data.owner}/${data.repo}/releases`)
         console.log(res)
-        if(res.stderr !== '')
+        if(res.stdout === '')
             throw new Error(res.stderr)
     }
 }
