55255a8b4b26eb8530ba4fbc90d834b73816a7bd
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-10-27 23:27:50 +0300

    ....

--- src/github.js
+++ src/github.js
@@ -39,7 +39,7 @@ export async function getGitHubVersions(data) {
  * @returns 
  */
 export async function getCurrentVersion(data) {
-    const githubResponse = (await github.getGitHubVersions(data))[0]
+    const githubResponse = (await getGitHubVersions(data))[0]
     if(!githubResponse.tag_name) {
         console.debug(githubResponse)
         throw new Error('tag_name value is undefined.')
